8903542f85df4e6c0e671d88f1bf2356429e9c1c
Benjamin <benjamin.cavy@gmail.com>
2017-06-16 01:50:46 +0200

    Add test setup

--- src/foo.test.js
+++ src/foo.test.js
@@ -1,5 +1,17 @@
+var fetchMock = require('fetch-mock');
+
 describe('Fake tests', () => {
   it('1 should equal 1', () => {
     expect(1).toBe(1)
+  })
+})
+
+describe('async call', ()  => {
+  it('should work with async', (done) => {
+    fetchMock.get('*', {hello: 'world'});
+    fetch('http://foo.bar')
+      .then(res => res.json())
+      .then(json => expect(json.hello).toBe('world'))
+      .then(done)
   })
 })
