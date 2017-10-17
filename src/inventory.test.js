47fa86476032ab35edfcb11202a890228ae1d827
Gauthier Poulet <gauthier.poulet@gmail.com>
2017-10-17 18:02:24 +0200

    add Jest unit tests

---
+++ src/inventory.test.js
@@ -0,0 +1,21 @@
+
+import AnimalContainer from './inventory'
+
+describe('my inventory should', () => {
+
+test('add animals', () => {
+  const inventory = new AnimalContainer();
+  inventory.addAnimal("Ramses", "Chat", "Sphynx", 8, "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Sphinx2_July_2006.jpg/100px-Sphinx2_July_2006.jpg")
+
+  const animals = inventory.getAnimals();
+  expect(animals.length).toBe(1)
+
+  const item = animals[0];
+  expect(item.id).toBe(0)
+  expect(item.name).toBe("Ramses")
+  expect(item.species).toBe("Chat")
+  expect(item.race).toBe("Sphynx")
+  expect(item.age).toBe(8)
+  expect(item.picture).toBe("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Sphinx2_July_2006.jpg/100px-Sphinx2_July_2006.jpg")
+});
+});
