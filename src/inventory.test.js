cf596fea59bef4dcf6d1e312d49bc00cc005111e
Gauthier Poulet <gauthier.poulet@gmail.com>
2017-10-18 10:39:18 +0200

    add another Jest unit test

--- src/inventory.test.js
+++ src/inventory.test.js
@@ -18,4 +18,16 @@ test('add animals', () => {
   expect(item.age).toBe(8)
   expect(item.picture).toBe("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Sphinx2_July_2006.jpg/100px-Sphinx2_July_2006.jpg")
 });
+
+test('remove animals', () => {
+  const inventory = new AnimalContainer();
+  inventory.addAnimal("Ramses", "Chat", "Sphynx", 8, "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Sphinx2_July_2006.jpg/100px-Sphinx2_July_2006.jpg")
+
+  const animals = inventory.getAnimals();
+  expect(animals.length).toBe(1)
+  expect(animals[0].id).toBe(0)
+
+  inventory.removeAnimal(0)
+  expect(animals.length).toBe(1)
+});
 });
